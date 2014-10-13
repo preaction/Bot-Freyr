dcb7fc30067ccc4939af23b1874c13fd96ce083d
Doug Bell <madcityzen@gmail.com>
2014-10-13 17:16:35 -0500

    initial skeleton

---
+++ lib/Freyr.pm
@@ -0,0 +1,22 @@
+package Freyr;
+# ABSTRACT: Write a sentence about what it does
+
+use strict;
+use warnings;
+
+
+
+1;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 ARGUMENTS
+
+=head1 OPTIONS
+
+=head1 ATTRIBUTES
+
+=head1 METHODS
