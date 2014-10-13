eaf1d8a8fedd5a5436439ecb04cbfb4ba15b05b6
Doug Bell <madcityzen@gmail.com>
2014-10-13 17:32:38 -0500

    let's go with Perl 5.20! fight the system!

--- lib/Freyr.pm
+++ lib/Freyr.pm
@@ -1,10 +1,31 @@
 package Freyr;
-# ABSTRACT: Write a sentence about what it does
+# ABSTRACT: IRC bot with multi-network and web API
 
 use strict;
 use warnings;
+use parent 'Import::Base';
 
+sub modules {
+    my ( $class, $bundles, $args ) = @_;
 
+    # Modules that are always imported
+    my @modules = (
+        'strict',
+        'warnings',
+        'feature' => [qw( :5.20 )],
+        'experimental' => [qw( signatures postderef )],
+    );
+
+    # Optional bundles
+    my %bundles = (
+        Test => [qw( Test::More Test::Deep Test::Differences )],
+    );
+
+    # Return an array of imports/unimports
+    return $class->SUPER::modules( $bundles, $args ),
+        @modules,
+        map { @{ $bundles{ $_ } } } grep { exists $bundles{ $_ } } @$bundles;
+}
 
 1;
 __END__
