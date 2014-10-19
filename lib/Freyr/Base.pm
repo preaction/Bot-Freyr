77440cbb287a997575c6d77d5b4585b781378687
Doug Bell <madcityzen@gmail.com>
2014-10-19 18:44:15 -0500

    add base Class and Role bundles
    Also write some better docs for Base modules. I might use this as
    boilerplate for Import::Base...

--- lib/Freyr/Base.pm
+++ lib/Freyr/Base.pm
@@ -18,8 +18,20 @@ sub modules {
     );
 
     # Optional bundles
+    my @class_common = (
+        'Types::Standard' => [qw( :all )],
+    );
+
     my %bundles = (
-        Test => [qw( Test::More Test::Deep Test::Differences )],
+        Test => [qw( Test::More Test::Deep Test::Differences Test::Exception )],
+        Class => [
+            qw( Moo::Lax ),
+            @class_common,
+        ],
+        Role => [
+            qw( Moo::Role::Lax ),
+            @class_common,
+        ],
     );
 
     if ( grep { $_ eq 'Test' } @$bundles ) {
@@ -41,10 +53,8 @@ __END__
     package MyModule;
     use Freyr::Base;
 
-    # Classes
     use Freyr::Base 'Class';
-
-    # Tests
+    use Freyr::Base 'Role';
     use Freyr::Base 'Test';
 
 =head1 DESCRIPTION
@@ -71,6 +81,50 @@ Currently the 5.20 feature bundle
 =item L<experimental> 'signatures' 'postderef'
 
 We are using the 5.20 experimental signatures and postfix deref syntax.
+
+=back
+
+=head1 BUNDLES
+
+The following bundles are available. You may import one or more of these by name.
+
+=head2 Class
+
+The class bundle makes your package into a class and includes:
+
+=over 4
+
+=item L<Moo::Lax>
+
+=item L<Types::Standard> ':all'
+
+=back
+
+=head2 Role
+
+The role bundle makes your package into a role and includes:
+
+=over 4
+
+=item L<Moo::Role::Lax>
+
+=item L<Types::Standard> ':all'
+
+=back
+
+=head2 Test
+
+The test bundle enables Mojo::IRC's test mode and includes:
+
+=over 4
+
+=item L<Test::More>
+
+=item L<Test::Deep>
+
+=item L<Test::Differences>
+
+=item L<Test::Exception>
 
 =back
 
