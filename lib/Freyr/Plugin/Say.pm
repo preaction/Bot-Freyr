b2cdcd6c9656c2b8841df5f34785b1bad99dffd5
Doug Bell <madcityzen@gmail.com>
2014-11-08 00:35:10 -0600

    add docs to the say plugin

--- lib/Freyr/Plugin/Say.pm
+++ lib/Freyr/Plugin/Say.pm
@@ -19,3 +19,39 @@ sub speak( $self, $msg ) {
 }
 
 1;
+
+=head1 SYNOPSIS
+
+    use Freyr;
+    use Freyr::Plugin::Say;
+
+    my $plugin = Freyr::Plugin::Say->new;
+
+    # Create a bot with our plugin
+    my $bot = Freyr->new(
+        nick => 'freyr',
+        prefix => '!',
+        host => 'irc.freenode.net',
+        channels => [ '#freyr' ],
+        plugins => {
+            say => $plugin,
+        },
+    );
+    Mojo::IOLoop->start;
+
+=head1 DESCRIPTION
+
+This plugin simply parrots anything said to it
+
+=head1 COMMANDS
+
+=head2 say [-t|--to <user|channel>] text
+
+    say Hello
+    say -t #freyr Hello
+    say -t preaction Hello
+
+Say the given C<text> to the given C<user> or C<channel>. By default, will send the text
+back to the same place the command was given (to the channel, or to the user in private
+message).
+
