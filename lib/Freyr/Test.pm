385cf1cb79249e24f3d05af472e6c68c6c12a00b
Doug Bell <madcityzen@gmail.com>
2014-11-09 22:32:24 -0600

    allow multiple tests per irc message

--- lib/Freyr/Test.pm
+++ lib/Freyr/Test.pm
@@ -2,21 +2,24 @@ package Freyr::Test;
 # ABSTRACT: Test functions for Freyr
 
 use Freyr::Base 'Test';
+use List::Util qw( pairs );
 use Sub::Exporter -setup => {
     exports => [qw( test_irc_msg )],
 };
 
-=sub test_irc_msg( $irc, $send, $test, $recv )
+=sub test_irc_msg( $irc, $send, @tests )
 
 Send an IRC message over the $irc object and test the routing of the message.
 
-$send is a message to send over IRC using Mojo::IRC's offline testing method. You must
-include the full IRC message, like:
+$send is a message to send over IRC using Mojo::IRC's offline testing method.
+You must include the full IRC message, like:
 
     :nick!user@host PRIVMSG #defocus Hello!
 
-$test must be either C<like> or C<unlike>, and L<Test::More>'s L<Test::More/like|like> or 
-L<Test::More/unlike> will be used.
+@tests is a list of pairs of tests ($test) and values to test against ($recv)
+
+$test must be either C<like> or C<unlike>, and L<Test::More>'s
+L<Test::More/like|like> or L<Test::More/unlike> will be used.
 
 $recv must be a string or regexp to test.
 
@@ -26,16 +29,21 @@ Also checks to make sure that common route callback problems do not occur.
 
 =cut
 
-sub test_irc_msg( $irc, $send, $test, $recv ) {
+sub test_irc_msg( $irc, $send, @tests ) {
     return sub {
         $irc->{to_irc_server} = '';
         $irc->from_irc_server( $send . "\r\n" );
-        if ( $test eq 'like' ) {
-            like $irc->{to_irc_server}, qr{$recv\r\n};
+
+        for my $t ( pairs @tests ) {
+            my ( $test, $recv ) = @$t;
+            if ( $test eq 'like' ) {
+                like $irc->{to_irc_server}, qr{$recv\r\n};
+            }
+            elsif ( $test eq 'unlike' ) {
+                unlike $irc->{to_irc_server}, qr{$recv\r\n};
+            }
         }
-        elsif ( $test eq 'unlike' ) {
-            unlike $irc->{to_irc_server}, qr{$recv\r\n};
-        }
+
         unlike $irc->{to_irc_server}, qr{\Q$irc\E\r\n},
             'returned IRC object is not spoken';
         $irc->{to_irc_server} = '';
